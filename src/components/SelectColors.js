2831c4b02f317de6213b3d64cdd5d59276ddfa34
sangho235 <sangho235@it.gsshop.com>
2021-07-13 23:17:52 +0900

    15.4.2 static contextType 사용하기 클래스로 변경

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ColorConsumer from '../contexts/color';
+import React,{Component} from 'react';
+import ColorContext from '../contexts/color';
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const SelectColors = () => {
-    return (
-        <div>
-            <h2>색상을 선택하세요</h2>
-            <ColorConsumer>
-                {({actions}) => (
+class SelectColors extends Component {
+    static contextType = ColorContext;
+
+    handleSetColor = color => {
+        this.context.actions.setColor(color);
+    };
+
+    handleSetSubColor = subcolor => {
+        this.context.actions.setSubcolor(subcolor);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>색상을 선택하세요</h2>
                     <div style={{ display : 'flex'}}>
                         {colors.map(color => (
                             <div
@@ -15,19 +25,18 @@ const SelectColors = () => {
                                 style={{
                                     background: color, width : '24px', height: '24px', cursor:'pointer'
                                 }}
-                                onClick={()=>actions.setColor(color)}
-                                onContextMenu = {e => {
-                                    e.preventDefault(); //마우스 오른쪽 버튼 클릭 시메뉴가 뜨는 것을 무시함
-                                    actions.setSubcolor(color);
+                                onClick={() => this.handleSetColor(color)}
+                                onContextMenu={e=>{
+                                    e.preventDefault();
+                                    this.handleSetSubColor(color);
                                 }}
                             />
                         ))}
                     </div>
-                )}
-            </ColorConsumer>
-            <hr/>
-        </div>
-    );
-};
+                <hr/>
+            </div>
+        );
+    }
+}
 
 export default SelectColors;
